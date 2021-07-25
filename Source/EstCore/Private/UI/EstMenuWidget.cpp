5801008808fc3eab45327358f331276e09eb61bd
Alan Edwardes <alan@alanedwardes.com>
2021-07-25 11:35:58 +0100

    Fixes race condition in UEstMenuWidget::AsyncNavigate

--- Source/EstCore/Private/UI/EstMenuWidget.cpp
+++ Source/EstCore/Private/UI/EstMenuWidget.cpp
@@ -41,6 +41,8 @@ void UEstMenuWidget::Action(FEstMenuAction Action)
 
 void UEstMenuWidget::AsyncNavigate(TSoftClassPtr<UEstMenuSection> MenuSection, FName Context)
 {
+	RemoveMenu();
+	
 	TWeakObjectPtr<UEstMenuWidget> WeakThis(this);
 
 	FSoftObjectPath StreamingObjectPath = MenuSection.ToSoftObjectPath();
@@ -60,8 +62,6 @@ void UEstMenuWidget::AsyncNavigate(TSoftClassPtr<UEstMenuSection> MenuSection, F
 		}
 	},
 	FStreamableManager::AsyncLoadHighPriority);
-
-	RemoveMenu();
 }
 
 void UEstMenuWidget::Navigate(UEstMenuSection* MenuSection, FName Context)
