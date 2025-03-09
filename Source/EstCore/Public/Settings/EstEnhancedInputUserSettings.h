1510faf9d505590068144c05722f87ccefdd0e2e
Alan Edwardes <alan@alanedwardes.com>
2025-03-09 20:07:22 +0000

    Adds EstEnhancedInputUserSettings
    (cherry picked from commit 4ea38a70fc029a1b16b5cadc7e944a49979c115b)

---
+++ Source/EstCore/Public/Settings/EstEnhancedInputUserSettings.h
@@ -0,0 +1,17 @@
+// Estranged is a trade mark of Alan Edwardes.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UserSettings/EnhancedInputUserSettings.h"
+#include "EstEnhancedInputUserSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESTCORE_API UEstEnhancedInputUserSettings : public UEnhancedInputUserSettings
+{
+	GENERATED_BODY()
+	
+};
